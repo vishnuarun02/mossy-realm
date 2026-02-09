6a984371afcbb8d2d00d90e245c1a9e15c8b202a
vishnuarun02 <vigilantstars6@gmail.com>
2026-02-08 16:31:52 -0800

    feat: add welcome gifs to homepage
    - Dancing frog top-right corner (desktop only, -6° tilt)
    - 'Welcome to internet' sign bottom-left (tablet+, -8° tilt)
    - Positioned outside content flow, won't block anything

--- mossy-realm/app/page.tsx
+++ mossy-realm/app/page.tsx
@@ -3,10 +3,33 @@ import SidebarRight from "@/components/SidebarRight";
 import MainPanel from "@/components/MainPanel";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="site-container">
+    <div className="site-container relative">
+      {/* Dancing frog - top right, peeking in */}
+      <div className="hidden lg:block absolute top-2 right-4 z-20 -rotate-6">
+        <Image 
+          src="/images/frog-welcome.gif" 
+          alt="welcome frog" 
+          width={100} 
+          height={75}
+          unoptimized
+        />
+      </div>
+
+      {/* Welcome sign - bottom left corner, tilted */}
+      <div className="hidden md:block absolute bottom-24 left-2 z-20 rotate-[-8deg]">
+        <Image 
+          src="/images/welcome-to-internet.gif" 
+          alt="welcome to the internet" 
+          width={90} 
+          height={30}
+          unoptimized
+        />
+      </div>
+
       {/* Header with title and nav */}
       <NavBar />
 
